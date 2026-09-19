95d0cd909415654caf4c2a517839d778dc611a90
Lingaraj Kunda <lingarajkunda@gmail.com>
2026-09-19 19:24:36 +0530

    tsconfig fix

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hay, I am running on AWS ECS Fargate with Docker image from AWS ECR",
+    });
+});
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "ok", message: "Server is healthy" });
+});
+exports.default = app;
